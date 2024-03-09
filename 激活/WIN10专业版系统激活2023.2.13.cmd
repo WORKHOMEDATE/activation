cc7c1b36f1925c211a980c5196bd1f850373dccc
WORKHOMEDATE <155273875+WORKHOMEDATE@users.noreply.github.com>
2024-03-09 08:29:38 +0800

    Add files via upload
    Signed-off-by: WORKHOMEDATE <155273875+WORKHOMEDATE@users.noreply.github.com>

---
+++ 激活/WIN10专业版系统激活2023.2.13.cmd
@@ -0,0 +1,5 @@
+slmgr.vbs /upk
+slmgr -ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+slmgr -skms kms.03k.org
+slmgr -ato
+slmgr -dlv
---
+++ 激活/Windows Server 2022 Datacenter 激活.cmd
@@ -0,0 +1,5 @@
+slmgr.vbs /upk
+slmgr -ipk WX4NM-KYWYW-QJJR4-XV3QB-6VM33
+slmgr -skms kms.03k.org
+slmgr -ato
+slmgr -dlv
